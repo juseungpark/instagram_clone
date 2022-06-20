8d3cbd00c8a5db1c6f47170099298529003392b1
juseung <parkjuseung0313@gmail.com>
2022-06-20 16:42:37 +0900

    login_activity implement

--- app/src/main/java/com/juseung/instagram_clone/LoginActivity.kt
+++ app/src/main/java/com/juseung/instagram_clone/LoginActivity.kt
@@ -1,26 +1,43 @@
 package com.juseung.instagram_clone
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.juseung.instagram_clone.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+
+    lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+
     private lateinit var binding: ActivityLoginBinding
 
 
     var auth: FirebaseAuth? = null
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
-
-
         val view = binding.root
         setContentView(view)
 
@@ -28,8 +45,52 @@ class LoginActivity : AppCompatActivity() {
         binding.emailLoginButton.setOnClickListener {
             signinAndSignup()
         }
+        binding.googleSignInButton.setOnClickListener {
+            activityResultLauncher.launch(googleSignInClient!!.signInIntent)
+
+        }
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("463168803840-m3ahs1b27l5sjcphj7ltcc2cdan1h87a.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result: ActivityResult ->
+            if (result.resultCode == RESULT_OK){
+                var task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                try {
+                    var account = task.getResult(ApiException::class.java)!!
+                    firebaseAuthWithGoogle(account.idToken!!)
+                    Log.d("GoogleLogin", "firebaseAuthWithGoogle: " + account.id)
+                } catch (e:ApiException){
+                    Log.d("GoogleLogin", "Google sign in failed: " + e.message)
+                }
+            }
+        }
+
     }
 
+
+
+
+
+
+    fun firebaseAuthWithGoogle(idToken: String){
+        var credential = GoogleAuthProvider.getCredential(idToken,null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    //Login
+                    moveMainPage(task.result.user)
+                } else {
+                    //show the error message
+                    Toast.makeText(this, task.exception!!.message, Toast.LENGTH_LONG).show()
+
+                }
+            }
+    }
     fun signinAndSignup() {
         auth?.createUserWithEmailAndPassword(
             binding.emailEdittext.text.toString(),
@@ -39,7 +100,7 @@ class LoginActivity : AppCompatActivity() {
                     task ->
                 if (task.isSuccessful) {
                     //Creating a user account
-                    moveMainPage(task.result.user)
+                    moveMainPage(task.result?.user)
                 } else {
                     //Login if you have account
                     signinEmail()
